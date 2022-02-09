ec01639cb3ed95f12984e0345b668601e888e2ad
RegineaW <73669527+RegineaW@users.noreply.github.com>
2022-02-09 18:24:56 -0500

    Trivia-Project-RegineaW

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,20 @@
 import React, { Component, useState } from "react";
 import "../css/App.css";
-import data from "../sample_data.json";
+import data from "../sample_data.json"; // ../ -> means 1 directory up from where we are
+
+function Question(props) {
+  return <div>{props.question}</div>;
+}
 
 function App() {
-  return <div className="app">Trivia!</div>;
+  let currentQuestionNum = 0;
+
+  return (
+    <div className="app">
+      Trivia!
+      <Question question="Question goes here" />
+    </div>
+  );
 }
 
 export default App;
